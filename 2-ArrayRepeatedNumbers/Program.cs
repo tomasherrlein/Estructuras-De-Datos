20be02c768f5903962a0922408dc38d13bc50d08
tomasherrlein <herrlein.tomas@gmail.com>
2025-07-05 22:53:11 -0300

    2 - Verificar si hay números repetidos en un array

---
+++ 2-ArrayRepeatedNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] numbers = { 1, 2, 3, 4 };
+
+System.Console.WriteLine(HasRepeatedNumbers(numbers));
+bool HasRepeatedNumbers(int[] array)
+{
+    var uniqueValues = new HashSet<int>();
+
+    foreach (var number in array)
+    {
+        if (!uniqueValues.Add(number))
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
